0a2ee8afba7ec1d3ac7918d212839c3b0fe6a848
Nathan Bateman <nathan.bateman.jr@gmail.com>
2014-11-23 15:14:24 +0700

    I have finished all coding but cannot get the .display method to work..awaiting a piazza response

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,5 @@
 
-var name = name;
+var name = "Nathan Bateman";
 var role = "Educator";
 var pic = "images/nathan.jpg";
 var formattedname = HTMLheaderName.replace("%data%",name);
@@ -14,21 +14,21 @@ var work = {
             "employer": "International Community School",
             "title": "Business/Computer Teacher",
             "location": "Bangkok, Thailand",
-            "dates worked": "2008-2015",
+            "datesworked": "2008-2015",
             "description": "Teaching Adobe Suite, Web Design, Marketing/Business, Subject Area Curriculum Team Chair"
         },
         {
             "employer": "Tillman Homes",
             "title": "Contractor's Assistant",
             "location": "Valdosta, GA",
-            "dates worked": "2004-2005, 2006-2007",
+            "datesworked": "2004-2005, 2006-2007",
             "description": "Helped oversee and prep sites for new homes.Requested city permits, Communicated and resolved issues with home owners and sub-contractors."
         },
         {
             "employer": "Liberty Place",
             "title": "Care Giver",
             "location": "Whitehall, MT",
-            "dates worked": "2005",
+            "datesworked": "2005",
             "description": "Personal care giver for residents with traumatic brain injuries.Prepared meals, gave showers, administered medication, and helped residents with various life skills."
         }
     ]
@@ -67,22 +67,15 @@ var projects = {
     "projects": [
         {
             "title": "Editing CSS Templates",
-            "images": ["edtech.jpg"],
-            "dates worked": "2012",
+            "images": ["images/edtech.jpg"],
+            "dates": "2012",
             "description": "Created lots of HTML/CSS projects in my EDTECH 502 class."
         },
         {
             "title": "Intro to HTML/CSS",
-            "images": ["project1.jpg"],
-            "dates worked": "2014",
+            "images": ["images/udacityp1.jpg"],
+            "dates": "2014",
             "description": "Project 1 for Udacity nano degree."
-        },
-        {
-            "employer": "Liberty Place",
-            "title": "Care Giver",
-            "location": "Whitehall, MT",
-            "dates worked": "2005",
-            "description": "Personal care giver for residents with traumatic brain injuries.Prepared meals, gave showers, administered medication, and helped residents with various life skills."
         }
     ]
 }
@@ -108,6 +101,28 @@ var bio = {
     ]
 }
 
+projects.display = function(){
+//function displayProjects(){
+    for (project in projects.projects){
+        $("#projects").append(HTMLprojectStart);
+        var formattedprojecttitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+        $(".project-entry:last").append(formattedprojecttitle);
+        var formattedprojectdates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+        $(".project-entry:last").append(formattedprojectdates);
+        var formattedprojectdescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+        $(".project-entry:last").append(formattedprojectdescription);
+        if (projects.projects[project].images.length > 0){
+            for (image in projects.projects[project].images){
+                var formattedprojectimage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedprojectimage);
+
+            }
+        }
+
+       
+    }
+};
+//displayProjects();
 function displayHeader(){
 if (bio.skills.length > 0){
 	$("#header").append(HTMLskillsStart);
@@ -125,11 +140,43 @@ if (bio.skills.length > 0){
 }
 };
 displayHeader();
-
+function displayWork(){
 for (job in work.jobs) {
 		$("#workExperience").append(HTMLworkStart);
 		var formattedemployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
 		var formattedjobtitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
 		var formattedemployertitle = formattedemployer + formattedjobtitle;
-		$(".work-entry:last").append(formattedemployertitle);
-	};
+        $(".work-entry:last").append(formattedemployertitle);
+        var formattedlocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+        $(".work-entry:last").append(formattedlocation);
+        var formatteddates = HTMLworkDates.replace("%data%",work.jobs[job].datesworked);
+        $(".work-entry:last").append(formatteddates);
+        var formatteddescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+        $(".work-entry:last").append(formatteddescription);
+
+	}
+};
+displayWork();
+
+//click counter is here 
+$(document).click(function(loc) {
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
+});
+
+$("#main").append(internationalizeButton);
+var oldName = bio.name;
+function inName() {
+    var finalName = oldName;
+    var n = oldName.indexOf(" ");
+    var firstname = oldName.substring(0,n);
+    var changefirst = firstname.slice(0,1).toUpperCase();
+    var firstname = changefirst + firstname.slice(1).toLowerCase();
+    var lastname = oldName.substring(n);
+    var lastname = lastname.toUpperCase();
+    var finalName = firstname + lastname;
+    return finalName;
+
+};
